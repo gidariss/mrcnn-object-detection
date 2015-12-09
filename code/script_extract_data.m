68a3c275f83077f8adcaa3c1c4c635fc8a732d5a
Spyros <spyridon.gidaris@gmail.com>
2015-12-09 17:23:13 +0100

    add example image

--- code/script_extract_data.m
+++ code/script_extract_data.m
@@ -1,2 +1,8 @@
-script_extract_sem_seg_aware_features('test', '2007', 'gpu_id', 4);
+
+% extact the vgg16 conv5 image features from pascal voc 2007 test set
+script_extract_vgg16_conv_features('test', '2007', 'gpu_id', 1); 
+
+% extact the semantic segmentation aware CNN image features from the 
+% pascal voc 2007 test set
+script_extract_sem_seg_aware_features('test', '2007', 'gpu_id', 1);
 
